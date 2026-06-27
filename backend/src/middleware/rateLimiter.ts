53b41bc831fbcdc570b9c629a304ae6cd15f6708
Angera-Silas <angerasilas@gmail.com>
2026-06-27 18:05:16 +0300

    (fix): add missing uploads to dockerfile

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const authRateLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    max: 100, // limit per IP
+    max: 10, // limit per IP
     message: 'Too many requests, please try later.',
     standardHeaders: false, // Don't send RateLimit-* headers
     legacyHeaders: false, // Disable the X-RateLimit-* headers
